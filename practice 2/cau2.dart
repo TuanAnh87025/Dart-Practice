a4cc849f96da3d4b4700bbef3ff088775c5c6bb2
TuanAnh87025 <125423318+TuanAnh87025@users.noreply.github.com>
2023-03-15 07:41:49 +0700

    Add files via upload

---
+++ practice 2/cau2.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+void main() {
+  print("Enter a character:");
+  String? c  = stdin.readLineSync();
+  if(c=="a"|| c=="e"|| c=="i"|| c== "u" || c== "o"){
+    print("vowel");
+  }
+  else  print("consonant"); 
+}
